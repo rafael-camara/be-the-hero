0408675331dc8baeefa27b414010577772419b65
Rafael Camara <rafaelcamara071@outlook.com>
2020-03-29 18:39:03 -0300

    Back-end da aplicação

---
+++ backend/knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+module.exports = {
+  // Ambiente de desenvolvimento
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
+  },
+  // Ambiente de produção para o time de desenvolvimento (Simula a produção)
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+  // Ambiente de produção (Vai para o servidor online)
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+}
